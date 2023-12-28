09e3be2e15e4e45b959df04ac49206585bd0a87e
Aiman Fadillah <mlakece914@gmail.com>
2023-12-28 21:31:04 +0700

    memperbaiki bug history

--- frontend/src/page/History.jsx
+++ frontend/src/page/History.jsx
@@ -6,7 +6,7 @@ export default function History ({setAnime}) {
     const nav = useNavigate();
 
     useEffect(() => {
-        setAnime();
+        setAnime({});
     },[]);
 
     function removeHistory (index) {
