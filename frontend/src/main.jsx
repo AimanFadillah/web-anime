ccf78a937418c0bf42d20ce2ee3a98c8abdcb704
Aiman Fadillah <mlakece914@gmail.com>
2024-01-06 07:52:09 +0700

    memasang speedInsights di frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,11 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SpeedInsights/>
     <App />
   </React.StrictMode>,
 )
