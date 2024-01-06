a042f1e09e97ae127b359c58d0e036516faf3a68
Aiman Fadillah <mlakece914@gmail.com>
2024-01-06 09:38:39 +0700

    update server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.use(express.static("public")) 
+app.get("/assets/index-0ae749c7.js",(req,res) => res.sendFile(__dirname + "/public/assets/index-0ae749c7.js"));
+app.get("/assets/index-bc07d9ae.css",(req,res) => res.sendFile(__dirname + "/public/assets/index-bc07d9ae.css"));
+app.get("/assets/bootstrap-icons-5b2dd4d4.woff2",(req,res) => res.sendFile(__dirname + "/public/assets/bootstrap-icons-5b2dd4d4.woff2"));
+app.get("/assets/bootstrap-icons-d0346eea.woff",(req,res) => res.sendFile(__dirname + "/public/assets/bootstrap-icons-d0346eea.woff"));
+app.get("/favicon.ico",(req,res) => res.sendFile(__dirname + "/favicon.ico"));
 
 app.get("*",(req,res) => {
     return res.sendFile(__dirname + "/index.html")
