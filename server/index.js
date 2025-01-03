320193637e6d70c3b0782b431aea08b4a6a830a8
AimanFadillah <mlakece914@gmail.com>
2025-01-03 16:56:36 +0700

    [change] function

--- server/index.js
+++ server/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-function setAsset(path){
-    return app.get(path,(req,res) => res.sendFile(__dirname + "/public" + path));
+function setAsset(app,path){
+    app.get(path,(req,res) => res.sendFile(__dirname + "/public" + path));
 }
 
-setAsset("/assets/index-70c11242.js");
-setAsset("/assets/index-bc07d9ae.css");
-setAsset("/assets/bootstrap-icons-5b2dd4d4.woff2");
-setAsset("/assets/bootstrap-icons-d0346eea.woff");
-setAsset("/favicon.ico");
-setAsset("/style.css");
+setAsset(app,"/assets/index-70c11242.js");
+setAsset(app,"/assets/index-bc07d9ae.css");
+setAsset(app,"/assets/bootstrap-icons-5b2dd4d4.woff2");
+setAsset(app,"/assets/bootstrap-icons-d0346eea.woff");
+setAsset(app,"/favicon.ico");
+setAsset(app,"/style.css");
 
 app.get("*",(req,res) => {
     return res.sendFile(__dirname + "/index.html")
