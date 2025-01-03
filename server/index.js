bcb0e89a66a9bced6f04d0c3692434580b8e0161
AimanFadillah <mlakece914@gmail.com>
2025-01-03 17:03:49 +0700

    [change] get

--- server/index.js
+++ server/index.js
@@ -2,16 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-function setAsset(app,path){
-    app.get(path,(req,res) => res.sendFile(__dirname + "/public" + path));
-}
-
-setAsset(app,"/assets/index-70c11242.js");
-setAsset(app,"/assets/index-bc07d9ae.css");
-setAsset(app,"/assets/bootstrap-icons-5b2dd4d4.woff2");
-setAsset(app,"/assets/bootstrap-icons-d0346eea.woff");
-setAsset(app,"/favicon.ico");
-setAsset(app,"/style.css");
+app.get("/assets/index-70c11242.js",(req,res) => res.sendFile(__dirname + "/public" + "/assets/index-70c11242.js"));
+app.get("/assets/index-bc07d9ae.css",(req,res) => res.sendFile(__dirname + "/public" + "/assets/index-bc07d9ae.css"));
+app.get("/assets/bootstrap-icons-5b2dd4d4.woff2",(req,res) => res.sendFile(__dirname + "/public" + "/assets/bootstrap-icons-5b2dd4d4.woff2"));
+app.get("/assets/bootstrap-icons-d0346eea.woff",(req,res) => res.sendFile(__dirname + "/public" + "/assets/bootstrap-icons-d0346eea.woff"));
+app.get("/favicon.ico",(req,res) => res.sendFile(__dirname + "/public" + "/favicon.ico"));
+app.get("/style.css",(req,res) => res.sendFile(__dirname + "/public" + "/style.css"));
 
 app.get("*",(req,res) => {
     return res.sendFile(__dirname + "/index.html")
