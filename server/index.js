af0c2a16217ced4943782c721ef73b6b12d51ff4
AimanFadillah <mlakece914@gmail.com>
2025-01-05 20:57:33 +0700

    [change] css

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get("/assets/index-4879b786.js",(req,res) => res.sendFile(__dirname + "/public" + "/assets/index-4879b786.js"));
-app.get("/assets/index-bc07d9ae.css",(req,res) => res.sendFile(__dirname + "/public" + "/assets/index-bc07d9ae.css"));
+app.get("/assets/index-asdasdvwqf.css",(req,res) => res.sendFile(__dirname + "/public" + "/assets/index-asdasdvwqf.css"));
 app.get("/assets/bootstrap-icons-5b2dd4d4.woff2",(req,res) => res.sendFile(__dirname + "/public" + "/assets/bootstrap-icons-5b2dd4d4.woff2"));
 app.get("/assets/bootstrap-icons-d0346eea.woff",(req,res) => res.sendFile(__dirname + "/public" + "/assets/bootstrap-icons-d0346eea.woff"));
 app.get("/favicon.ico",(req,res) => res.sendFile(__dirname + "/public" + "/favicon.ico"));
