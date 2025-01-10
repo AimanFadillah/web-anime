ffbfd7ff631934e1c6e1f75f4d29ce3a76a53aed
AimanFadillah <mlakece914@gmail.com>
2025-01-10 20:47:00 +0700

    [change] vercel

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 4000;
 
-app.use(express.static("public"))
+// Melayani file statis dari folder "public"
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*",(req,res) => {
-    return res.sendFile(__dirname + "/index.html")
-})
+// Rute wildcard untuk menangani semua permintaan dan mengarahkan ke index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.listen(port,() => console.log("Serve On"))
+app.listen(port, () => console.log(`Server running on port ${port}`));
