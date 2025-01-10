0cdc7c850a748a90fbdc376142b70b254c9c7501
AimanFadillah <mlakece914@gmail.com>
2025-01-10 19:33:42 +0700

    [change] build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) {
+            return 'assets/[name].[hash].css';
+          }
+          return 'assets/[name].[hash].[ext]';
+        },
+      },
+    },
+  }
 })
