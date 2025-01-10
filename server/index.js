50163974c500699890fcf1b618c1767ed609229c
AimanFadillah <mlakece914@gmail.com>
2025-01-10 20:49:10 +0700

    [remove] index and vercel copy

--- server/index.js
+++ server/index.js
@@ -4,10 +4,8 @@ const path = require("path");
 const app = express();
 const port = 4000;
 
-// Melayani file statis dari folder "public"
 app.use(express.static(path.join(__dirname, "public")));
 
-// Rute wildcard untuk menangani semua permintaan dan mengarahkan ke index.html
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
